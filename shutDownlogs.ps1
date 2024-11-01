be916751894a2cecdf71f22679eeb72f7979afdf
User <Willspri@gmail.com>
2024-11-02 12:05:59 +1300

    committed

---
+++ shutDownlogs.ps1
@@ -0,0 +1,13 @@
+
+$dateInput = "2024-10-29" 
+
+$startOfMonth = (Get-Date $dateInput).AddDays(-(Get-Date $dateInput).Day + 1)
+$startOfNextMonth = $startOfMonth.AddMonths(1)
+
+
+$events = Get-EventLog -LogName System | Where-Object {
+	($_.EventID -eq 6006 -or $_.EventID -eq 6008 -or $_.EventID -eq 6005) -and 
+	$_.TimeGenerated -ge $startOfMonth -and $_.TimeGenerated -lt $startOfNextMonth
+}
+
+$events | Select-Object TimeGenerated, EventID, Message
